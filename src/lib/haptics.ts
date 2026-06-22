3620946cfcc98a54627f373c35e16256dcaad038
Pablo Portas López <81629707+TeenBiscuits@users.noreply.github.com>
2026-06-22 13:01:51 +0200

    comment: haptics not working on iOS

--- src/lib/haptics.ts
+++ src/lib/haptics.ts
@@ -1,3 +1,7 @@
+// There is and issue with this library, Apple has fixed the trick that it used to produce haptics, i will leave it
+// here but if you detect that it's not working it's not my fault
+// https://github.com/lochie/web-haptics/issues/41
+
 import { WebHaptics } from "web-haptics";
 
 const haptics = new WebHaptics();
